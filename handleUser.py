5f830be11a4bd00e8cd20c646bb88e7c6ab1e996
Aldo Berziu <berziu2004@gmail.com>
2024-05-30 22:44:22 +0200

    new updates

--- handleUser.py
+++ handleUser.py
@@ -3,16 +3,30 @@ import uuid
 user_table = {}
 
 def add_user():
-    name = input("Enter user name: ")
+    while True:
+        name = input("Enter user name: ")
+        if name in user_table:
+            print(f"'{name}' already exists in our database. Please
+enter a unique name.")
+        else:
+            break
+
     balance = float(input("Enter initial balance: "))
     id = uuid.uuid1()
     user_table[name] = {"name": name, "balance": balance, "id": id}
+    created_user = find_user(name)
+    print('createduser', created_user)
+    return created_user
 
-def find_user():
-    name = input("Insert your account name: ")
+def find_user(name):
+    if not name:
+        name = input("Insert your account name: ")
     user = user_table.get(name)
     if user:
-        return user_table.get(name)
+        return user
+    else:
+        print(f"User '{name}' not found in the database.")
+        return None
 
 def delete_user():
     name = input("Enter user name to delete: ")
--- index.py
+++ index.py
@@ -7,14 +7,12 @@ priority_queue = []
 
 def add_action(priority, action):
     heapq.heappush(priority_queue, (priority, action))
-    # print(f"Added action: {action} with priority: {priority}")
     print("Wait in the line for your time to come, based on priority")
 
 # Function to pop the highest priority action from the priority queue
 def pop_action():
     if priority_queue:
         priority, action = heapq.heappop(priority_queue)
-        print(f"Processed action: {action} with priority: {priority}")
         return action
     else:
         print("No actions in the queue.")
@@ -22,49 +20,60 @@ def pop_action():
 
 # building the queue
 def build_queue():
-        print("\n1. Create an account")
-        print("2. Check balance")
-        print("3. Deposit money")
-        print("4. Withdraw money")
-        print("5. Delete your account")
-        transaction_type = input('What type of transcation you wanna make?')
-        print("____________________________________________________________")
-        match(transaction_type):
-            case "1":
-                add_action(4, 'create')
-            case "2":
-                add_action(1, 'check')
-            case "3":
-                add_action(3, 'deposit')
-            case "4":
-                add_action(3, 'withdraw')
-            case "5":
-                add_action(2, 'delete')
+        transactions = int(input('How many transcations you wanna make?'))
+        actions = []
+        for i in range(transactions):
+            print("\n1. Create an account")
+            print("2. Check balance")
+            print("3. Deposit money")
+            print("4. Withdraw money")
+            print("5. Delete your account")
+            transaction_type = input('\nWhat type of transcation youwanna make? ')
+            actions.append(transaction_type)
+            # match(transaction_type):
+            #     case "1":
+            #         add_action(4, 'create')
+            #     case "2":
+            #         add_action(1, 'check')
+            #     case "3":
+            #         add_action(3, 'deposit')
+            #     case "4":
+            #         add_action(3, 'withdraw')
+            #     case "5":
+            #         add_action(2, 'delete')
+        add_action(transactions, actions)
 
 def main():
     while True:
-        n = input('how many people are waiting in the queue?')
+        n = input('\nhow many people are waiting in the queue? ')
         for i in range(int(n)):
             build_queue()
         if(len(priority_queue) > 0):
-            for i in priority_queue:
-                priority, action = i
-                match(action):
-                    case "create":
-                        print("______________________________________________________")
-                        add_user()
-                    case "check":
-                        print("______________________________________________________")
-                        user = find_user()
-                        if user:
-                            print(f"|  Name: {user['name']}  |  Balance: {user['balance']}  |  ID: {user['id']}")
-                        else:
-                            print("User not found.")
-                    case "deposit":
-                        transaction("deposit")
-                    case "withdraw":
-                        transaction("withdraw")
-                    case "delete":
-                        delete_user()
-                heapq.heappop(priority_queue)
+            for people in priority_queue:
+                priority, action = people
+                was_account_created = False
+                for act in action:
+                    match(act):
+                        case "1":
+                            print("\nYOU ARE CREATING YOUR ACCOUNT\n")
+                            created_account = add_user()
+                            was_account_created = True
+                        case "2":
+                            if was_account_created:
+                                print("\nYOU ARE CHECKING YOUR ACCOUNTBALANCE\n")
+                                print(created_account)
+                                # user = find_user()
+                                # if user:
+                                #     print(f"|  Name: {user['name']}|  Balance: {user['balance']}  |  ID: {user['id']}")
+                                # else:
+                                #     print("User not found.")
+                        case "3":
+                            print("\nYOU ARE DEPOSITING MONEY INTO YOUR ACCOUNT\n")
+                            transaction("deposit")
+                        case "4":
+                            transaction("withdraw")
+                        case "5":
+                            delete_user()
+                pop_action()
+
 main()
